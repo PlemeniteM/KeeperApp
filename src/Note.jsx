f585c3a13c1e29f7d308f92e580c38067c88d350
Parzival <parzivalnewton1@gmail.com>
2021-05-15 11:50:06 +0530

    Styles changes

--- src/Note.jsx
+++ src/Note.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Note(){
     return (<div className="note">
         <h1>This is Title</h1>
-        <p>This is Task</p>
+        <p> Taskhis is TaskTaskhis is TaskTaskhis is TaskTaskhis is TaskTaskhis is Task</p>
     </div>)
 }
 export default Note;
