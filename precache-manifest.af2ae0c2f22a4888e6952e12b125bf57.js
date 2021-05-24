6c51df813f4046e84532da37627967fadedbf45c
Parzival <parzivalnewton1@gmail.com>
2021-05-24 12:20:13 +0530

    Updates

---
+++ precache-manifest.af2ae0c2f22a4888e6952e12b125bf57.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e3990dc26148969a8780d5d276ac52",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "80199527fc045054768a",
+    "url": "/KeeperApp/static/js/2.39ba7f42.chunk.js"
+  },
+  {
+    "revision": "71240bd119c3c35c5fe8",
+    "url": "/KeeperApp/static/js/main.5e2b78db.chunk.js"
+  },
+  {
+    "revision": "9dc866653fcfda415af8",
+    "url": "/KeeperApp/static/js/runtime-main.f1d969fb.js"
+  }
+]);
